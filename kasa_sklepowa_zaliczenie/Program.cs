5408e78fc88f0847af05dfca942b155e1fe63e61
MAlicka1 <alicka.martyna@wp.pl>
2023-02-08 22:19:48 +0100

    Add project files.

---
+++ kasa_sklepowa_zaliczenie/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kasa_sklepowa_zaliczenie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
